3c12887709f6a3069a87cff02c4a1865c6bf4255
OzomaroC <ozomarocyril@gmail.com>
2022-11-03 20:46:10 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -> printing string in reverse using recursion
+ * @s: string s
+ */
+void _print_rev_recursion(char *s)
+	s = "lpa/\0";
+
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(S);
+	s--;
+	putchar(*s);
+}
