d52c500fe769e81308b426eca28bab6a1e1d971f
OzomaroC <ozomarocyril@gmail.com>
2022-11-03 21:01:12 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,5 +2,12 @@
 #define MAIN_H
 int _putchar(char c);
 void _puts_recursions(char *s);
+void _prints_rev_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_mumber(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif
