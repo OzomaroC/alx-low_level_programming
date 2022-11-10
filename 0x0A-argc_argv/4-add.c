7984e607984600040b32665925c070b5ddfa5f14
OzomaroC <ozomarocyril@gmail.com>
2022-11-09 20:11:34 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of commands used
+ * Return: 0 on Success
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, num, i, j, k;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0, argv[i][j] != '\0'; j++)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+	}
+
+	for (k = 0; k < argv; k++)
+	{
+		num = atoi(argv[k]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
