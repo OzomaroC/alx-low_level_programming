5c44a1234e6c2b2c229473c9c7f3d8c633f64f12
OzomaroC <ozomarocyril@gmail.com>
2022-11-09 18:46:52 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,8 @@
  * @argv: an array of command listed
  * Return: 0 for Success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
+	return (0);
 }
