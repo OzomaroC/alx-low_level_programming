e90298352f3200930841fb66fd72151524cef432
OzomaroC <ozomarocyril@gmail.com>
2022-11-09 20:19:22 -0800

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,16 +14,19 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 		printf("0\n");
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0, argv[i][j] != '\0'; j++)
 		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
 			printf("%s\n", "Error");
 			return (1);
+			}
 		}
 	}
 
-	for (k = 0; k < argv; k++)
+	for (k = 1; k < argv; k++)
 	{
 		num = atoi(argv[k]);
 		result += num;
